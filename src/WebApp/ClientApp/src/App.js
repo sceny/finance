64affe5f938e27a9eac69d4e891cf0133d00a8c5
Jenser Almeida <jenseralmeida@gmail.com>
2019-09-05 23:02:54 -0700

    Minimal start

---
+++ src/WebApp/ClientApp/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './custom.css'
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+        <span>Hello, world!</span>
+    );
+  }
+}
---
+++ src/WebApp/ClientApp/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    <App />,
+  rootElement);
+
+registerServiceWorker();
+
