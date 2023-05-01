4e3c1b3459955fbec9a6373074e0a2d8db1848e2
Jenser Almeida <jenseralmeida@gmail.com>
2023-05-01 11:22:11 -0700

    Trying, trying

---
+++ src/WebApp/ClientApp/src/app/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import {
+    Button,
+    makeStyles,
+    shorthands,
+    Title1,
+    tokens,
+} from '@fluentui/react-components';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const useStyles = makeStyles({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '200px',
+
+        ...shorthands.border('2px', 'dashed', tokens.colorPaletteBerryBorder2),
+        ...shorthands.borderRadius(tokens.borderRadiusMedium),
+        ...shorthands.gap('5px'),
+        ...shorthands.padding('10px'),
+    },
+});
+
+const Home: NextPage = () => {
+    const styles = useStyles();
+
+    return (
+        <>
+            <Head>
+                <title>My app</title>
+            </Head>
+
+            <div className={styles.container}>
+                <Title1>Hello world!</Title1>
+                <Button>A button</Button>
+            </div>
+        </>
+    );
+};
+
+export default Home;
--- src/WebApp/ClientApp/src/pages/_document.tsx
+++ src/WebApp/ClientApp/src/pages/_document.tsx
@@ -1,26 +1,46 @@
+'use client';
+
+import {
+    createDOMRenderer,
+    renderToStyleElements,
+} from '@fluentui/react-components';
 import Document, {
     Html,
     Head,
     Main,
     NextScript,
     DocumentContext,
-} from "next/document";
-import {
-    createDOMRenderer,
-    renderToStyleElements,
-} from "@fluentui/react-components";
+} from 'next/document';
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
+        // 👇 creates a renderer that will be used for SSR
         const renderer = createDOMRenderer();
-        const styles = renderToStyleElements(renderer);
+        const originalRenderPage = ctx.renderPage;
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) =>
+                    function EnhancedApp(props) {
+                        const enhancedProps = {
+                            ...props,
+                            // 👇 this is required to provide a proper renderer instance
+                            renderer,
+                        };
+
+                        return <App {...enhancedProps} />;
+                    },
+            });
+
         const initialProps = await Document.getInitialProps(ctx);
+        const styles = renderToStyleElements(renderer);
 
         return {
             ...initialProps,
             styles: (
                 <>
                     {initialProps.styles}
+                    {/* 👇 adding Fluent UI styles elements to output */}
                     {styles}
                 </>
             ),
@@ -29,7 +49,7 @@ class MyDocument extends Document {
 
     render() {
         return (
-            <Html lang="en">
+            <Html>
                 <Head />
                 <body>
                     <Main />
