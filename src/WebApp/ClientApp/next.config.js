2e50832ca8f55b9baeba2ad1ccd3d7b319358e1e
Jenser Almeida <jenseralmeida@gmail.com>
2023-06-08 02:55:10 -0700

    std nextjs template

--- src/WebApp/ClientApp/next.config.js
+++ src/WebApp/ClientApp/next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: false,
-    experimental: {
-        appDir: true,
-    },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
