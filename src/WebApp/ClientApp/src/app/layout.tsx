e2415988322f8063301a3c254635b6c839b338c2
Jenser Almeida <jenseralmeida@outlook.com>
2025-11-03 10:17:15 -0800

    x

--- src/WebApp/ClientApp/src/app/layout.tsx
+++ src/WebApp/ClientApp/src/app/layout.tsx
@@ -1,6 +1,30 @@
 'use client';
 
-import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
+import React from 'react';
+import {
+    FluentProvider,
+    webDarkTheme,
+    makeStyles,
+} from '@fluentui/react-components';
+import { Text } from '@fluentui/react-text';
+import { Divider } from '@fluentui/react-divider';
+import { Card, CardHeader, CardPreview } from '@fluentui/react-card';
+
+const useStyles = makeStyles({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    item: {
+        margin: 5,
+        minWidth: 300,
+        '@media (max-width: 600px)': {
+            width: '100%',
+        },
+    },
+});
+
 
 export default function RootLayout({
     children,
@@ -10,9 +34,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <FluentProvider theme={teamsLightTheme}>
-                    {children}
-                </FluentProvider>
+                <FluentProvider theme={webDarkTheme}>{children}</FluentProvider>
             </body>
         </html>
     );
--- src/WebApp/ClientApp/src/app/page.tsx
+++ src/WebApp/ClientApp/src/app/page.tsx
@@ -2,15 +2,15 @@
 
 import {
     Button,
-    FluentProvider,
     makeStyles,
     shorthands,
-    teamsLightTheme,
     Title1,
     tokens,
 } from '@fluentui/react-components';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { Text } from '@fluentui/react-text';
+import { Card, CardHeader, CardPreview } from '@fluentui/react-card';
 
 const useStyles = makeStyles({
     container: {
@@ -33,6 +33,50 @@ const Home: NextPage = () => {
             <Head>
                 <title>My app</title>
             </Head>
+
+            <div className={styles.container}>
+                <div className={styles.item}>
+                    <Card>
+                        <CardHeader>
+                            <Text >Card 1</Text>
+                        </CardHeader>
+                        <CardPreview>
+                            <Text>
+                                Fluent UI is a collection of utilities, React
+                                components, and tokens for building web
+                                applications.
+                            </Text>
+                        </CardPreview>
+                    </Card>
+                </div>
+                <div className={styles.item}>
+                    <Card>
+                        <CardHeader>
+                            <Text variant="large">Card 2</Text>
+                        </CardHeader>
+                        <CardPreview>
+                            <Text>
+                                Fluent UI provides you with a robust and
+                                flexible toolset to create seamless experiences
+                                in less time.
+                            </Text>
+                        </CardPreview>
+                    </Card>
+                </div>
+                <div className={styles.item}>
+                    <Card>
+                        <CardHeader>
+                            <Text variant="large">Card 3</Text>
+                        </CardHeader>
+                        <CardPreview>
+                            <Text>
+                                Fluent UI includes a full suite of SharePoint
+                                web parts, Office Add-ins, Teams apps, and more.
+                            </Text>
+                        </CardPreview>
+                    </Card>
+                </div>
+            </div>
 
             <div className={styles.container}>
                 <Title1>Hello world!</Title1>
